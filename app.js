d3b3309c2f352bbb3a8a10bc485bdd7e341349c0
carlosbarondev <carlosbarondev@gmail.com>
2021-10-15 21:13:33 +0200

    Borrado agregado

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 require('colors');
 
-const { guardarDB } = require('./helpers/guardarArchivo');
+const { guardarDB, leerDB } = require('./helpers/guardarArchivo');
 const { inquirerMenu,
     pausa,
-    leerInput
+    leerInput,
+    listadoTareasBorrar,
+    confirmar
 } = require('./helpers/inquirer');
 const Tareas = require('./models/tareas');
 //const { mostrarMenu, pausa } = require('./helpers/mensajes');
@@ -12,6 +14,12 @@ const main = async () => {
 
     let opt = '';
     const tareas = new Tareas();
+
+    const tareasDB = leerDB();
+
+    if (tareasDB) { // cargar tareas
+        tareas.cargarTareasFromArray(tareasDB);
+    }
 
     do {
         // Imprimir menu
@@ -25,11 +33,27 @@ const main = async () => {
                 tareas.crearTarea(desc);
                 break;
             case '2':
-                console.log(tareas.listadoArr);
+                tareas.listadoCompleto();
+                break;
+            case '3': // listar completadas
+                tareas.listarPendientesCompletadas(true);
+                break;
+            case '4': // listar pendientes
+                tareas.listarPendientesCompletadas(false);
+                break;
+            case '6': // Borrar
+                const id = await listadoTareasBorrar(tareas.listadoArr);
+                if (id !== '0') {
+                    const ok = await confirmar('¿Esta seguro?');
+                    if (ok) {
+                        tareas.borrarTarea(id);
+                        console.log('Tarea borrada');
+                    }
+                }
                 break;
         }
 
-        //guardarDB(tareas.listadoArr);
+        guardarDB(tareas.listadoArr);
 
         if (opt !== "0") await pausa();
     } while (opt !== '0');
